19e3a883b0716dbad25bfd4d1e3e53879e64722f
unknown <thiti.mwk.main@gmail.com>
2020-02-26 22:07:01 +0700

    :zap: Custom error middleware has updated and applied to routes , but with the remain of traces which are commented

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,7 +9,8 @@ exports.getBootcamps = async (req, res, next) => {
 		const bootcamps = await Bootcamp.find();
 		res.status(200).json({ sucess: true, data: bootcamps, count: bootcamps.length });
 	} catch (err) {
-		res.status(400).json({ success: false });
+		//res.status(400).json({ success: false });
+		next(err);
 	}
 };
 
@@ -27,7 +28,8 @@ exports.getBootcamp = async (req, res, next) => {
 		res.status(200).json({ success: true, data: bootcamp });
 	} catch (err) {
 		//res.status(400).json({ success: false });
-		next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+		//next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+		next(err);
 	}
 };
 
@@ -43,7 +45,8 @@ exports.createBootcamp = async (req, res, next) => {
 			data: bootcamp
 		});
 	} catch (err) {
-		res.status(400).json({ success: false });
+		//res.status(400).json({ success: false });
+		next(err);
 	}
 };
 
@@ -58,12 +61,14 @@ exports.updateBootcamp = async (req, res, next) => {
 		});
 
 		if (!bootcamp) {
-			return res.status(400).json({ success: false });
+			//return res.status(400).json({ success: false });
+			return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 		}
 
 		res.status(200).json({ success: true, data: bootcamp });
 	} catch (err) {
-		res.status(400).json({ success: false });
+		//res.status(400).json({ success: false });
+		next(err);
 	}
 };
 
@@ -75,11 +80,13 @@ exports.deleteBootcamp = async (req, res, next) => {
 		const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
 		if (!bootcamp) {
-			return res.status(400).json({ success: false });
+			//return res.status(400).json({ success: false });
+			return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 		}
 
 		res.status(200).json({ success: true, data: {} });
 	} catch (err) {
-		res.status(400).json({ success: false });
+		//res.status(400).json({ success: false });
+		next(err);
 	}
 };
