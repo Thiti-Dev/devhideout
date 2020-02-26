1d8bfdae105c8b2a7a94cd40235f63edb08cbca7
unknown <thiti.mwk.main@gmail.com>
2020-02-26 19:12:02 +0700

    :zap: Added colors , applied to the log

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 //import custom logger middleware
 //const logger = require('./middleware/logger');
@@ -30,12 +31,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-	console.log(`The server is currently running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+	console.log(`The server is currently running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-	console.log(`Error: ${err.message}`);
+	console.log(`Error: ${err.message}`.red);
 	// Close server & exit process
 	server.close(() => process.exit(1));
 });
