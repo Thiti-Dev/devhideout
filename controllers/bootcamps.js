1ac7183c0af47d5628eeab81a16f008c81913f1b
unknown <thiti.mwk.main@gmail.com>
2020-03-03 00:30:38 +0700

    :zap::fire::bulb: Added reverse populate(virtual) , cascade delete middleware in bootcamp model

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -25,7 +25,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`);
 
 	// Finding resource
-	query = Bootcamp.find(JSON.parse(queryStr));
+	query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
 	// Select Fields
 	if (req.query.select) {
@@ -117,12 +117,14 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   DELETE /api/v1/bootcamps/:id
 // @acess   Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-	const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+	const bootcamp = await Bootcamp.findById(req.params.id); // findByIdAndDelete not gonna triggered the middle ware
 
 	if (!bootcamp) {
 		//return res.status(400).json({ success: false });
 		return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 	}
+
+	bootcamp.remove();
 
 	res.status(200).json({ success: true, data: {} });
 });
