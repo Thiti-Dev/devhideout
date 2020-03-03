36da51487b444cf079f94261b2b3e1900f368c9a
unknown <thiti.mwk.main@gmail.com>
2020-03-03 21:34:07 +0700

    :zap: Added photo upload for bootcamp, :bulb: file type|size filtered

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
@@ -153,5 +154,49 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
 		success: true,
 		count: bootcamps.length,
 		data: bootcamps
+	});
+});
+
+// @desc    Upload photo for bootcamp
+// @route   PUT /api/v1/bootcamps/:id/photo
+// @acess   Private
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+	const bootcamp = await Bootcamp.findById(req.params.id); // findByIdAndDelete not gonna triggered the middle ware
+
+	if (!bootcamp) {
+		//return res.status(400).json({ success: false });
+		return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+	}
+
+	if (!req.files) {
+		return next(new ErrorResponse(`Please upload a file`, 400));
+	}
+
+	const file = req.files.file;
+
+	// Make sure the image is a photo
+	if (!file.mimetype.startsWith('image')) {
+		return next(new ErrorResponse(`Please upload an image file`, 400));
+	}
+
+	// Check filesize
+	if (file.size > process.env.MAX_FILE_UPLOAD) {
+		return next(new ErrorResponse(`Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`, 400));
+	}
+
+	// Create custom filename
+	file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
+
+	file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+		if (err) {
+			console.log(err);
+			return next(new ErrorResponse(`Problem with file upload`, 500));
+		}
+
+		await Bootcamp.findByIdAndUpdate(req.params.id, {
+			photo: file.name
+		});
+
+		res.status(200).json({ success: true, data: file.name });
 	});
 });
