a74707b6122d208004d18f7f65d7b299c5441aa3
unknown <thiti.mwk.main@gmail.com>
2020-03-03 17:14:41 +0700

    :zap: Added update course, delete course

--- controllers/courses.js
+++ controllers/courses.js
@@ -51,7 +51,7 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 
 	const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 	if (!bootcamp) {
-		return next(new ErrorResponse(`No bootcamp course with the id of ${req.params.bootcampId}`), 404);
+		return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`), 404);
 	}
 
 	const course = await Course.create(req.body);
@@ -59,5 +59,42 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 	res.status(200).json({
 		success: true,
 		data: course
+	});
+});
+
+// @desc    Update course
+// @route   POST /api/v1/courses/:id
+// @acess   Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+	let course = await Course.findById(req.params.id);
+	if (!course) {
+		return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+	}
+
+	course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true
+	});
+
+	res.status(200).json({
+		success: true,
+		data: course
+	});
+});
+
+// @desc    Delete course
+// @route   DELETE /api/v1/courses/:id
+// @acess   Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+	const course = await Course.findById(req.params.id);
+	if (!course) {
+		return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+	}
+
+	await course.remove();
+
+	res.status(200).json({
+		success: true,
+		data: {}
 	});
 });
