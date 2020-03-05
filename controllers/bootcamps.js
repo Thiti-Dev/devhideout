c1bc3427bcf6159a363aeef80439d7ecf89563d2
unknown <thiti.mwk.main@gmail.com>
2020-03-05 15:14:20 +0700

    :zap: Add the user rights to update/delete the bootcamp and also add photo

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -134,6 +134,11 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
 		return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 	}
 
+	// Make sure user is bootcamp owner
+	if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		return next(new ErrorResponse(`User ${req.user.id} is not authorized to delete this bootcamp`, 401));
+	}
+
 	if (!req.files) {
 		return next(new ErrorResponse(`Please upload a file`, 400));
 	}
